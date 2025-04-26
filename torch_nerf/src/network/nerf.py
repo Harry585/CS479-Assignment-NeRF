1b1905e68b2bb1afb43dbfbcc085be7e7a7fede4
Harry Wu <harry8.wu@gmail.com>
2025-04-26 02:33:15 +0000

    implemented Task1 [untested]

--- torch_nerf/src/network/nerf.py
+++ torch_nerf/src/network/nerf.py
@@ -8,6 +8,7 @@ from typeguard import typechecked
 from jaxtyping import Float, jaxtyped
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class NeRF(nn.Module):
@@ -33,9 +34,29 @@ class NeRF(nn.Module):
         Constructor of class 'NeRF'.
         """
         super().__init__()
-
-        # TODO
-        raise NotImplementedError("Task 1")
+        self.pos_dim = pos_dim
+        self.view_dir_dim = view_dir_dim
+        self.feat_dim = feat_dim
+        layer_sizes = [pos_dim, feat_dim, feat_dim, feat_dim, feat_dim, feat_dim + pos_dim,  \
+                       feat_dim, feat_dim, feat_dim, ]
+        self.MLP = nn.ModuleList([
+            nn.Linear(pos_dim, feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+            nn.Linear(feat_dim, feat_dim + pos_dim),
+            nn.Linear(feat_dim + pos_dim, feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+            nn.Linear(feat_dim, feat_dim),
+        ])
+        self.predict_sigma = nn.Linear(feat_dim, 1)
+        self.no_activation_layer = nn.Linear(feat_dim, feat_dim)
+        self.predict_rgb = nn.Sequential(
+                nn.Linear(feat_dim + view_dir_dim, 128),
+                nn.ReLU(),
+                nn.Linear(128, 3),
+                nn.Sigmoid()
+        )
 
     @jaxtyped
     @typechecked
@@ -58,6 +79,27 @@ class NeRF(nn.Module):
             sigma: The density predictions evaluated at the given sample points.
             radiance: The radiance predictions evaluated at the given sample points.
         """
+        # NOTE: we use a skip connection, so we must splice in the input
+        h = pos
+        for i, layer in enumerate(self.MLP[:4]):
+            h = layer(h)
+            print(f"layer {i+1}: h shape={h.shape}")
+            h = F.relu(h)
+        # Splice the position input in again at layer 5
+        h = torch.cat([h, pos], dim=-1)
+        for i, layer in enumerate(self.MLP[4:7]):
+            h = layer(h)
+            print(f"layer {i+1}: h shape={h.shape}")
+            h = F.relu(h)
 
-        # TODO
-        raise NotImplementedError("Task 1")
+        # Get sigma
+        sigma = F.relu(self.predict_sigma(h))
+
+        # Get rgb values
+        h = self.no_activation_layer(h)
+        h = torch.cat([h, view_dir], dim=-1)
+        rgb = self.predict_rgb(h)
+        return sigma, rgb
+
+
+
